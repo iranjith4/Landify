1c88d12cf1eaf87a87e0eb579879d0798af3f91f
Ranjithkumar Matheswaran <iranjith4@gmail.com>
2015-04-25 12:28:21 +0530

    Initial Commit

---
+++ Landify/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Landify
+//
+//  Created by Ranjith on 25/04/15.
+//  Copyright (c) 2015 RanjithKumar Matheswaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
